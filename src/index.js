d7ac1b100c58cdd78ede8cba6d143b6c488c6b11
Bekjon <bekjonishpulatov8@gmail.com>
2023-02-07 11:20:02 +0500

    add navbar and hamburger toggle class

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const hamburger = document.querySelector(".hamburger");
+const navbar = document.querySelector("nav");
+const links = document.querySelectorAll(".nav-link");
+
+console.log(hamburger);
+
+hamburger.addEventListener("click", function (e) {
+  console.log("clicked");
+  hamburger.classList.toggle("is-active");
+  navbar.classList.toggle("open");
+
+  console.log(hamburger.classList);
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    console.log("link clicked");
+    // hamburger.classList.toggle("is-active");
+    // navLinks.classList.toggle("open");
+  });
+});
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
