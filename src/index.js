94e20bdba4cecd6484f4b916f9302c4bf11a6858
Bekjon <bekjonishpulatov8@gmail.com>
2023-02-08 14:31:40 +0500

    Implement mobile menu

--- src/index.js
+++ src/index.js
@@ -2,26 +2,21 @@ const hamburger = document.querySelector('.hamburger');
 const navbar = document.querySelector('nav');
 const links = document.querySelectorAll('.nav-link');
 
-console.log(hamburger);
-
-hamburger.addEventListener('click', (e) => {
-  console.log('clicked');
+hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('is-active');
   navbar.classList.toggle('open');
 });
 
 links.forEach((link) => {
-  link.addEventListener('click', (e) => {
-    console.log('link clicked');
-    // hamburger.classList.toggle("is-active");
-    // navLinks.classList.toggle("open");
+  link.addEventListener('click', () => {
+    navbar.classList.toggle('open');
+    hamburger.classList.toggle('is-active');
   });
 });
 
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener('click', function (e) {
+  anchor.addEventListener('click', (e) => {
     e.preventDefault();
-
     document.querySelector(this.getAttribute('href')).scrollIntoView({
       behavior: 'smooth',
     });
