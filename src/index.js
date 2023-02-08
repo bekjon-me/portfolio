7385d76902d2034aee87267cdfb3b94c6a8fc660
Bekjon <bekjonishpulatov8@gmail.com>
2023-02-08 13:32:21 +0500

    more z-index for hamburger

--- src/index.js
+++ src/index.js
@@ -1,31 +1,29 @@
-const hamburger = document.querySelector(".hamburger");
-const navbar = document.querySelector("nav");
-const links = document.querySelectorAll(".nav-link");
+const hamburger = document.querySelector('.hamburger');
+const navbar = document.querySelector('nav');
+const links = document.querySelectorAll('.nav-link');
 
 console.log(hamburger);
 
-hamburger.addEventListener("click", function (e) {
-  console.log("clicked");
-  hamburger.classList.toggle("is-active");
-  navbar.classList.toggle("open");
-
-  console.log(hamburger.classList);
+hamburger.addEventListener('click', (e) => {
+  console.log('clicked');
+  hamburger.classList.toggle('is-active');
+  navbar.classList.toggle('open');
 });
 
 links.forEach((link) => {
-  link.addEventListener("click", (e) => {
-    console.log("link clicked");
+  link.addEventListener('click', (e) => {
+    console.log('link clicked');
     // hamburger.classList.toggle("is-active");
     // navLinks.classList.toggle("open");
   });
 });
 
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
+  anchor.addEventListener('click', function (e) {
     e.preventDefault();
 
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
     });
   });
 });
