f4c0937ce3655f4a38b239c8dee9a354f240f5ac
Bekjon <bekjonishpulatov8@gmail.com>
2023-02-08 15:03:46 +0500

    Implement scroll highlight for the desktop version

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 const hamburger = document.querySelector('.hamburger');
 const navbar = document.querySelector('nav');
 const links = document.querySelectorAll('.nav-link');
+const sections = document.querySelectorAll('section');
+const navLi = document.querySelectorAll('nav > ul li');
+
+window.onscroll = () => {
+  var current = '';
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (scrollY >= sectionTop - 60) {
+      current = section.getAttribute('id');
+    }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove('active');
+    if (li.classList.contains(current)) {
+      li.classList.add('active');
+    }
+  });
+};
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('is-active');
@@ -15,7 +34,7 @@ links.forEach((link) => {
 });
 
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener('click', (e) => {
+  anchor.addEventListener('click', function (e) {
     e.preventDefault();
     document.querySelector(this.getAttribute('href')).scrollIntoView({
       behavior: 'smooth',
