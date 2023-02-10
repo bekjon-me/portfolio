358667d9309bf9d149cc45ebdd64aa972af05709
Bekjon <bekjonishpulatov8@gmail.com>
2023-02-10 10:28:40 +0500

    Implement mobile modal

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ const links = document.querySelectorAll('.nav-link');
 const sections = document.querySelectorAll('section');
 const navLi = document.querySelectorAll('nav > ul li');
 const allSeeProjectBtns = document.querySelectorAll('#works button');
-const modalContainer = document.querySelector('.modalContainer');
 const modal = document.querySelector('.modal');
-const close = document.querySelector('.close');
+const closeModalBtn = document.querySelector('.close');
+const modalH2 = document.querySelector('.modal-header h2');
+const modalText = document.querySelector('.modal-body p');
+const languages = document.querySelectorAll('.modal-header ul');
+const image = document.querySelector('.modal-body img');
 
 console.log(allSeeProjectBtns);
 
@@ -14,7 +17,7 @@ window.onscroll = () => {
   let current = '';
   sections.forEach((section) => {
     const sectionTop = section.offsetTop;
-    if (window.scrollY >= sectionTop) {
+    if (window.scrollY >= sectionTop - 300) {
       current = section.getAttribute('id');
     }
   });
@@ -39,14 +42,85 @@ links.forEach((link) => {
   });
 });
 
-allSeeProjectBtns.forEach((btn) => {
-  btn.addEventListener('click', () => {
-    modal.classList.add('open');
-    modalContainer.classList.add('blur');
-  });
+closeModalBtn.addEventListener('click', () => {
+  modal.classList.toggle('open');
+  modal.classList.toggle('blur');
 });
 
-close.addEventListener('click', () => {
-  modal.classList.remove('open');
-  modalContainer.classList.remove('blur');
+const projects = [
+  {
+    name: 'Multi-Post Stories',
+    text: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    image: './src/assets/Img Placeholder.svg',
+    technologies: ['css', 'html', 'bootstrap', 'ruby'],
+    live: 'https://www.google.com/',
+    source: 'https://www.google.com/',
+  },
+  {
+    name: 'Professional Art Printing Data',
+    text: " A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    image: './src/assets/worksBg.svg',
+    technologies: ['html', 'bootstrap', 'ruby'],
+    live: 'https://www.google.com/',
+    source: 'https://www.google.com/',
+  },
+  {
+    name: 'Professional Art Printing Data',
+    text: " A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    image: './src/assets/worksBg.svg',
+    technologies: ['html', 'bootstrap', 'ruby'],
+    live: 'https://www.google.com/',
+    source: 'https://www.google.com/',
+  },
+  {
+    name: 'Professional Art Printing Data',
+    text: " A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    image: './src/assets/worksBg.svg',
+    technologies: ['html', 'bootstrap', 'ruby'],
+    live: 'https://www.google.com/',
+    source: 'https://www.google.com/',
+  },
+  {
+    name: 'Professional Art Printing Data',
+    text: " A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    image: './src/assets/worksBg.svg',
+    technologies: ['html', 'bootstrap', 'ruby'],
+    live: 'https://www.google.com/',
+    source: 'https://www.google.com/',
+  },
+  {
+    name: 'Professional Art Printing Data',
+    text: " A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    image: './src/assets/worksBg.svg',
+    technologies: ['html', 'bootstrap', 'ruby'],
+    live: 'https://www.google.com/',
+    source: 'https://www.google.com/',
+  },
+  {
+    name: 'Professional Art Printing Data',
+    text: " A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    image: './src/assets/worksBg.svg',
+    technologies: ['html', 'bootstrap', 'ruby'],
+    live: 'https://www.google.com/',
+    source: 'https://www.google.com/',
+  },
+];
+
+allSeeProjectBtns.forEach((btn, mainIndex) => {
+  btn.addEventListener('click', () => {
+    console.log(mainIndex);
+    modal.classList.toggle('open');
+    modal.classList.toggle('blur');
+    modalH2.textContent = projects[mainIndex].name;
+    modalText.textContent = projects[mainIndex].text;
+    languages.forEach((language, index) => {
+      language.innerHTML = '';
+      projects[mainIndex].technologies.forEach((tech) => {
+        const li = document.createElement('li');
+        li.textContent = tech;
+        language.appendChild(li);
+      });
+    });
+    image.src = projects[mainIndex].image;
+  });
 });
